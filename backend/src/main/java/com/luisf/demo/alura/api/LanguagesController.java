fbe2ce3676ca18a3082ddcedce2f8e95e50c1adf
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2022-07-23 05:49:03 -0300

    monorepo, CRUD, váriaves de ambiente, mongodb, aula entregue

--- backend/src/main/java/com/luisf/demo/alura/api/LanguagesController.java
+++ backend/src/main/java/com/luisf/demo/alura/api/LanguagesController.java
@@ -1,67 +1,43 @@
 package com.luisf.demo.alura.api;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LanguagesController {
 
-        private List<Language> languages = List.of(
-                        new Language("1", "c",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/c/c_256x256.png",
-                                        "10.0"),
-                        new Language("2", "csharp",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/csharp/csharp_256x256.png",
-                                        "7.0"),
-                        new Language("3", "css",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/css/css_256x256.png",
-                                        "9.0"),
-                        new Language("4", "go-old",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/go-old/go-old_256x256.png",
-                                        "6.8"),
-                        new Language("5", "go",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/go/go_256x256.png",
-                                        "8.0"),
-                        new Language("6", "haskell",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/haskell/haskell_256x256.png",
-                                        "7.0"),
-                        new Language("7", "html",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/html/html_256x256.png",
-                                        "9.6"),
-                        new Language("8", "java",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png",
-                                        "9.0"),
-                        new Language("9", "javascript",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/javascript/javascript_256x256.png",
-                                        "9.0"),
-                        new Language("10", "kotlin",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/kotlin/kotlin_256x256.png",
-                                        "9.0"),
-                        new Language("11", "lua",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/lua/lua_256x256.png",
-                                        "7.0"),
-                        new Language("12", "php",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png",
-                                        "9.0"),
-                        new Language("13", "python",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/python/python_256x256.png",
-                                        "8.5"),
-                        new Language("14", "r",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/r/r_256x256.png",
-                                        "5.0"),
-                        new Language("15", "ruby",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/ruby/ruby_256x256.png",
-                                        "6.0"),
-                        new Language("16", "swift",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/swift/swift_256x256.png",
-                                        "7.0"),
-                        new Language("17", "typescript",
-                                        "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/typescript/typescript_256x256.png",
-                                        "9.0"));
+  @Autowired
+  private LanguageRepository repository;
 
-        @GetMapping("/languages")
-        public List<Language> getLanguages() {
-                return languages;
-        }
+  @GetMapping("/languages")
+  public List<Language> getLanguages() {
+    List<Language> languages = repository.findAll(Sort.by(Sort.Direction.DESC, "rating"));
+    // languages.add(new Language("id", "title", "urlImage", "rating"));
+    return languages;
+  }
+
+  @GetMapping("/languages-remove")
+  public String getLanguagesRemove(String id) {
+    try {
+      repository.deleteById(id);
+      return "Ok";
+    } catch (IllegalArgumentException e) {
+      return e.getMessage();
+    }
+  }
+
+  @PostMapping("/languages")
+  public ResponseEntity<Language> postLanguages(@RequestBody Language language) {
+    // languages.add(new Language("id", "title", "urlImage", "rating"));
+    Language postLanguage = repository.save(language);
+    return new ResponseEntity<Language>(postLanguage, HttpStatus.CREATED);
+  }
 }
